ccd0fec16b9e74d4a3f7a52d985c99165c4bfe78
sor-ee <suntimethpw@gmail.com>
2022-10-22 01:43:54 +0700

    edit by soree

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -14,11 +14,46 @@
         <li><a href="{{ URL::to('home') }}"><i class="fa fa-home"></i> หน้าร้าน</a></li>
         <li class="active">Customer</li>
     </div>
-
-    {{-- @if(count($cart_items)) --}}
-
     <div class="panel panel-default">
-   
+        <div class="panel-heading">
+            <div class="panel-title">
+                <strong>รายการ</strong>
+            </div>
+        </div>
+        <div class="panel-body">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Username</th>
+                        <th>email</th>
+                        <th>วันที่สร้าง</th>
+                        <th>การทำงาน</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($data_users as $people)
+                        <tr>
+                            <td>{{ $people->id }}</td>
+                            <td>{{ $people->name }}</td>
+                            <td>{{ $people->email }}</td>
+                            <td>{{ $people->created_at }}</td>
+                            <td>
+                                <a href="/customer/action/{{ $people->id }}" class="btn btn-info">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                <button class="btn_delete btn btn-danger">
+                                    <input type="hidden" class="product_id" value="{{ $people->id }}">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+                <tfoot>
+                </tfoot>
+            </table>
+        </div>
 
     </div>
 
