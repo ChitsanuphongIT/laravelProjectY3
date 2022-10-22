4b08243122357dee1fa61f4db70d1e39f15f8246
Saharat Thongin <6306021620191@fitm.kmutnb.ac.th>
2022-10-22 07:24:00 +0700

    Done create Order and Order_Detail to Database

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\Order_Detail;
+use Carbon\Carbon;
 
 class OrderController extends Controller
 {
@@ -20,8 +21,49 @@ class OrderController extends Controller
         return view('order.tempDetail',compact('order_details'));
     }
 
-    public function finish(){
+    // Create order in database
+    public function create(Request $body){
+    
+        $order = new Order();
+        $order->user_id = $body->cust_userid;
+        $order->order_ref = $this->create_order_id();
+        $order->status = 0;
+        $order->save();
 
+        foreach (json_decode($body->jsonDataCart) as $p){
+            $order_details = new Order_Detail();
+            $order_details->order_id = $order->id;
+            $order_details->product_id = $p->id;
+            $order_details->qty = $p->qty;
+            $order_details->price = $p->price;
+            $order_details->total = $p->price * $p->qty;
+            $order_details->save();
+        }
+
+        return 0;
+    }
+
+
+
+    // Created use function for generating order id
+    public function create_order_id(){
+        //format "PO20180218####"
+        //format "POYYYYMMDD####" 14 characters
+
+        $latestOrder = Order::orderBy('created_at','DESC')->first();
+        
+        if($latestOrder->created_at->format('Y-m-d') != Carbon::now()->format('Y-m-d')){
+            $orderDigit = "0001";
+        }else{
+            $latestDigit = substr($latestOrder->order_ref, 10, 4);
+            $orderDigit = sprintf('%04s', ((int)$latestDigit)+1); 
+
+        }
+
+        $order_id = 'PO'.date('Ymd').$orderDigit;
+        return $order_id;
+
+       
     }
 
 }
