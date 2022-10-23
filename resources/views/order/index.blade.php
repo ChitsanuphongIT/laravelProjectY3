424717b01049e82f5e5b3c4d9d9989a2322f97e3
Nutchanock Wisuttiphun <6306021620131@fitm.kmutnb.ac.th>
2022-10-23 18:10:56 +0700

    update order index eiei

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -18,13 +18,44 @@
     {{-- @if(count($cart_items)) --}}
 
     <div class="panel panel-default">
-        
-        <ul>
+        <div class="panel-heading">
+            <div class="panel-title">
+                <strong>รายการสั่งซื้อสินค้า</strong>
+            </div>
+        </div>
+
+        <div class="panel-body">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>OrderID</th>
+                        <th>เลขที่ใบสั่งซื้อ</th>
+                        <th>ชื่อลูกค้า</th>
+                        <th>วันที่สั่งซื้อสินค้า</th>
+                        <th>รายละเอียด</th>
+                        <th>สถานะการชําระเงิน</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($orders as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->order_ref }}</td>
+                            <td>{{ $item->user_id }}</td>
+                            <td>{{ $item->created_at }}</td>
+                            <td><a href="{{ URL::to('detail/'.$item->id )}}">รายละเอียด</a></td>
+                            <td>{{ $item->status}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        {{-- <ul>
             @foreach ($orders as $item)
                 <li> <a href="{{URL::to('order/detail/'.$item->id)}}"> {{json_encode($item)}}  </li>
             @endforeach
             
-        </ul>
+        </ul> --}}
 
     </div>
 
